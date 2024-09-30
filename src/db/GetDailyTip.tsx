f06a892c2349467efe6c5d28826f30e4abe54580
Alon Joshua <alonjoshua@gmail.com>
2024-09-30 23:59:21 +0300

    Refactor daily tip to routeLodaer

---
+++ src/db/GetDailyTip.tsx
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+export const GetDailyTip = async () => {
+  const prisma = new PrismaClient();
+  const collectionData = await prisma.$runCommandRaw({
+    collStats: "Tip"
+  });
+
+  const totalCount = collectionData.count as number;
+
+  const dayDate = new Date();
+  const dayOfYear = Math.floor(
+    (Number(dayDate) - Number(new Date(dayDate.getFullYear(), 0, 0))) / 86400000
+  );
+
+  const response = await prisma.tip.findFirst({
+    take: 1,
+    skip: dayOfYear % totalCount,
+  });
+
+  return response;
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,29 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import { Menu } from "../components/contextual/menu/menu";
 import { Intro } from "../components/contextual/intro/intro";
 import { TipOfTheDay } from "~/components/contextual/tip-of-the-day/tip-of-the-day";
-import { SectionSpacer } from "~/components/reusable/section-spacer/section-spacer";
+import { SectionSpacer } from "~/components/reusable/section-spacer";
 import { TipsSearch } from "~/components/contextual/tips-search/tips-search";
+import { routeLoader$ } from '@builder.io/qwik-city';
+import { GetDailyTip } from "~/db/GetDailyTip";
+
+export const useDailyTip = routeLoader$(() => {
+  return GetDailyTip();
+});
+
 export default component$(() => {
+
+  const dailyTip = useDailyTip();
+
   return (
     <>
       <Menu />
       <Intro />
       <SectionSpacer />
-      <TipOfTheDay />
+      {dailyTip.value ? (
+        <TipOfTheDay tip={dailyTip.value} />
+      ) : (
+        <div>No tip available for today.</div>
+      )}
       <SectionSpacer />
       <TipsSearch />
     </>
